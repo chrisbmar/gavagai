0faa08e1f7e48e4c1903f8da50923be4f216a5f6
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2019-08-28 17:52:20 +0200

    setup file structure and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 class="heading">Find out what Semantic Words there are for any word in your language by harvesting the power of Gavagai's Living Lexicon that is continously learning new words.</h1>
+      <h3>Clicking on a word will show specific information relating to that word.</h3>
+      <div>  
+        <h2 class="results-label">Semantically similar words will appear below</h2>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 1000px;
+}
+
+@import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+
+html {
+  font-size: 18px;
+  font-family: 'Raleway', sans-serif;
+  color: white;
+}
+
+body {
+  background-image: url("./assets/backgroundimage-min.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.heading {
+  width: 670px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  margin-top: 85px;
+  color: black;
+  opacity: 0.7;
+}
+
+h2 {
+  width: 670px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  margin-top: 50px;
+  opacity: 0.4;
+}
+
+h3 {
+  width: 670px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  margin-top: 10px;
+  color: white;
+  font-weight: 800;
+  opacity: 0.6;
+}
+
+@media only screen and (max-width: 730px) { 
+  .heading {
+    font-size: 27px;
+    width: 90%;
+  }
+
+  h3 {
+    color: grey;
+    opacity: 0.8;
+  }
+
+  body {
+    background-image: none;
+  }
+
+ .results-label {
+    display: none;
+  }
+
+  h3 {
+    width: 90%;
+    margin-top: 10px;
+  }
+}
+
+@media only screen and (max-width: 730px) {
+  .heading {
+  margin-top: 10px;
+  }
 }
 </style>
